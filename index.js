bce37beffc2f2fbe0b58d30c2a3dfbf1e5bf6f87
Caroline Lanford <caroline.lanford@gmail.com>
2018-03-15 00:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(){
+  drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver(){
+  drivers.unshift("Bob")
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+
+function appendDriver(){
+  newDrivers = [drivers.push("Ralph")] 
+  newDrivers
+}
